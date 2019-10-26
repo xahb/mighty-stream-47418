9033b61f42dbbe93da050b35ebb328c3b11da92f
xahb <xahb@bk.ru>
2019-10-26 16:23:30 +0300

    runtime.txt added
    Former-commit-id: 870a7ae122ae70d7b719682918bf9dc0ab9c413f

--- clahelpbot.py
+++ clahelpbot.py
@@ -67,9 +67,10 @@ def write_photo(message,memebase=memebase):
     #cur = conn.cursor()
     #cur.execute("INSERT INTO memebase (num, data) VALUES (%s, %s)", ())
     new_row = {'message_id':message.message_id,'from_user':message.from_user,'date':message.date,'chat':message.chat,'id':memebase.shape[0]}
-    memebase = memebase.append(pd.DataFrame(new_row,index=[memebase.shape[0]]))
-    with open('memebase.pickle', 'wb') as mb:
-        pickle.dump(memebase, mb, protocol=2)
+#    memebase = memebase.append(pd.DataFrame(new_row,index=[memebase.shape[0]]))
+#    with open('memebase.pickle', 'wb') as mb:
+#        pickle.dump(memebase, mb)
+    bot.reply_to(message, 'Сохранил'+str(new_row.message_id))
 
 @bot.message_handler(func=lambda message: message.chat.type=='private', content_types=['text'])
 def private_message(message):
