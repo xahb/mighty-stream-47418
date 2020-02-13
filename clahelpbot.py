e2d2d8d22766a8ee8e027bd29082947103673c70
xahb <xahb@bk.ru>
2020-02-14 01:42:26 +0300

    fix keyboard response
    Former-commit-id: 51e907333467b974abd73bf57a7d4aae473fe913

--- clahelpbot.py
+++ clahelpbot.py
@@ -94,12 +94,17 @@ def private_message(message):
         emoji_challengers = [random_choice(list(EMOJI_UNICODE)) for i in range(20)]
         reaction_args = [response.id, message.chat.id, str(emoji_challengers)]
         reaction_buttons = []
+        reaction_args_full = reaction_args.copy()
+        reaction_args_full.append(emoji_challengers[0])
+        keyboard.add(telebot.types.InlineKeyboardButton(text=emojize(emoji_challengers[0]), callback_data=json.dumps(reaction_args_full)))
+        '''
         for emoji in emoji_challengers:
             reaction_args_full = reaction_args.copy()
             reaction_args_full.append(emoji)
             reaction_buttons.append(telebot.types.InlineKeyboardButton(text=emojize(emoji), callback_data=json.dumps(reaction_args_full)))
+        '''
         #в след строке дичь, потому что метод add не принимает list или tuple, а каждую кнопку считает отдельным аргументом. Надо подумать, как с этим быть
-        keyboard.add(reaction_buttons[0])#,reaction_buttons[1],reaction_buttons[2],reaction_buttons[3],reaction_buttons[4])
+        #keyboard.add(reaction_buttons[0],reaction_buttons[1],reaction_buttons[2],reaction_buttons[3],reaction_buttons[4])
         bot.send_message(message.chat.id, 'Этот мем как:', reply_markup=keyboard)
         try:
             sql_chat = session.query(SqlChat).filter_by(id=message.chat.id).first()
