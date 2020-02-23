4bee052669cb9c7118f9a82eeb8dda45abb7b313
xahb <xahb@bk.ru>
2020-02-23 20:08:21 +0300

    clean version
    Former-commit-id: b86961beb56f9fd4456bc736f0a1f7c3a868c4c8

--- clahelpbot.py
+++ clahelpbot.py
@@ -83,9 +83,9 @@ def show_stats(message):
     session = Session()
     try:
         sql_chat = session.query(SqlChat).filter_by(id=message.chat.id).first()
-        sql_chat.messages_shared  = 2 #session.query(func.count(SqlMessage)).filter_by(chat_id=message.chat.id)
-        sql_chat.messages_received  = 2 #session.query(func.count(SqlKeyReaction)).filter_by(reaction_chat_id=message.chat.id)
-        sql_chat.messages_rated  = 2 #session.query(func.count(SqlKeyReaction)).filter(SqlKeyReaction.reaction_chat_id==message.chat.id, SqlKeyReaction.emoji_winner!='0')
+        sql_chat.messages_shared  = session.query(func.count(SqlMessage.id)).filter_by(chat_id=message.chat.id)
+        sql_chat.messages_received  = session.query(func.count(SqlKeyReaction.id)).filter_by(reaction_chat_id=message.chat.id)
+        sql_chat.messages_rated  = session.query(func.count(SqlKeyReaction.id)).filter(SqlKeyReaction.reaction_chat_id==message.chat.id, SqlKeyReaction.emoji_winner!='0')
     except:
         sql_chat = SqlChat(message)
         session.add(sql_chat)
